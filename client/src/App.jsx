2b5a0a13beba0ac293daa8b03226bce9f73df2de
Your Name <you@example.com>
2024-02-12 21:39:25 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-500">app</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/Project.jsx
@@ -0,0 +1,8 @@
+
+export default function Project() {
+  return (
+    <div>
+      Project
+    </div>
+  )
+}
