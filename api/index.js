25e371177a55c7798777cbfaf039218808f0d27d
Your Name <you@example.com>
2024-02-25 08:18:47 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
-const app =express()
+dotenv.config()
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!!!');
-})
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("MongoDB is Connected");
+  }).catch(err=>{
+    console.log(err);
+  })
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!!!");
+});
