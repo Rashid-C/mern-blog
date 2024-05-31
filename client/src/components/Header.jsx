ca37252755b0e48355e3caaa5293ed4ce0c5d11b
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2024-06-01 03:14:12 +0530

    complete dark mode fuctionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,23 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector,useDispatch } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice.js";
+
 export default function Header() {
   const path = useLocation().pathname;
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
+
   return (
-    <Navbar className="boarder-b-2">
+    <Navbar className='border-b-2'>
       <Link
         to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
           Rashid
         </span>
         Blog
@@ -27,12 +30,18 @@ export default function Header() {
           className="hidden lg:inline"
         />
       </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
-      <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=>dispatch(toggleTheme())}>
-          <FaMoon />
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
         {currentUser ? (
           <Dropdown
@@ -52,9 +61,7 @@ export default function Header() {
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item>
-              Sign out
-            </Dropdown.Item>
+            <Dropdown.Item>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to="/sign-in">
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,14 @@ import "./index.css";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { ThemeModeScript } from "flowbite-react";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <ThemeModeScript>
+      <ThemeProvider>
         <App />
-      </ThemeModeScript>
+      </ThemeProvider>
     </Provider>
-    ,
   </PersistGate>
 );
