6920c3bb15415b7340e54c844e07a801e926d148
Your Name <anurashid11055>
2024-05-31 15:14:14 +0530

    test

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,11 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice.js";
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch=useDispatch()
   const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="boarder-b-2">
@@ -29,7 +31,7 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=>dispatch(toggleTheme())}>
           <FaMoon />
         </Button>
         {currentUser ? (
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { ThemeModeScript } from "flowbite-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <App />
+      <ThemeModeScript>
+        <App />
+      </ThemeModeScript>
     </Provider>
     ,
   </PersistGate>
