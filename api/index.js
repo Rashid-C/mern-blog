197476356c9d803aac1671afb1944282dd47154f
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2024-08-12 23:56:09 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+// 
 import path from "path";
 
 dotenv.config();
@@ -18,7 +19,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+// 
 const __dirname = path.resolve();
 
 const app = express();
@@ -34,9 +35,9 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
-
+// 
 app.use(express.static(path.join(__dirname, "/client/dist")));
-
+// 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
